c4dde89964fd4d56d9c42991e09c7084af654ffe
gavin Lyons <glyons66@hotmail.com>
2017-08-20 18:30:09 +0100

    version 1.4

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 
 setup(
-    name="tvdoon",
-    version="1.3.0",
-    author="LinArcX",
-    author_email="linarcx@gmail.com",
+    name="rpi_tempmon.py",
+    version="1.4",
+    author="gavin lyons",
+    author_email="glyons66@hotmail.com",
     description="Display the ARM CPU and GPU temperature of Raspberry Pi",
     license=" GPL",
-    keywords="PI, Raspberry, CPU , ARM, GPU",
+    keywords="PI Raspberry CPU ARM GPU,temperature",
     url="https://github.com/LinArcX/tvdoon",
     download_url='https://github.com/gavinlyonsrepo/raspeberrypi_tempmon/archive/1.4.tar.gz',
     packages=['rpiTempSrc','rpiTempMod',],
     install_requires= ['matplotlib','pip'],
     setup_requires = ['pip'],
-    scripts=['rpiTempSrc/rpi_tempmon.py],
+    scripts=['rpiTempSrc/rpi_tempmon.py'],
     classifiers=[
         "Development Status :: 3 - RTM",
         "Topic :: Utilities",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.4",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
